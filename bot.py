61b5fd99a599c4a89a30fe93dae348dae8505662
antdepin <antdepin@gmail.com>
2026-03-15 18:19:49 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,27 @@
+
 import requests
 import time
 
-TOKEN="8687563836:AAE8RXxf2-UWxbr7U5cFscJ8Bri6NflIS6Q"
-CHAT_ID="178689360"
+TOKEN = "1292804066:AAHIGsAOWz3vBXF4RJBnnQGH9m2UgNfJhek"
+CHAT_ID = "178689360"
 
 def send(msg):
-    url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url,data={"chat_id":CHAT_ID,"text":msg})
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": msg}, timeout=20)
+
+print("BOT LIVE AVVIATO")
 
 while True:
-
     try:
+        # controllo sito live
+        r = requests.get("https://www.nowgoal.com/", timeout=20)
+        text = r.text.lower()
 
-        r=requests.get("https://www.nowgoal.com/football/live")
-
-        if "football" in r.text:
-
-            print("scanner live")
+        if "football" in text:
+            send("⚡ Scanner live attivo")
 
         time.sleep(30)
 
-    except:
-
+    except Exception as e:
+        print("Errore:", e)
         time.sleep(30)
