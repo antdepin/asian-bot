6ed889a41fe4b4d776ea3b0280fc8db899459b66
antdepin <antdepin@gmail.com>
2026-03-15 18:59:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,9 @@
-import requests
-import time
-
-TOKEN = "1292804066:AAHIGsAOWz3vBXF4RJBnnQGH9m2UgNfJhek"
-CHAT_ID = "178689360"
-
-def send(msg):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": msg}, timeout=20)
-
-print("BOT PARTITO")
-
-send("🚀 Bot LIVE avviato su Railway")
-
 while True:
     try:
         print("Scanner attivo")
+        send("scanner attivo")
         time.sleep(30)
+
     except Exception as e:
         print(e)
         time.sleep(30)
