696ecb62e56f9b9059bf6bcdfbb21bc6b28d4cdb
antdepin <antdepin@gmail.com>
2026-03-15 20:46:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,13 +9,48 @@ def send(msg):
     requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
 
 print("BOT PARTITO")
-send("BOT PARTITO")
+send("⚽ Asian Scanner LIVE attivo")
 
 while True:
     try:
-        print("scanner attivo")
-        send("scanner attivo")
-        time.sleep(30)
+
+        url = "https://api.sofascore.com/api/v1/sport/football/events/live"
+        r = requests.get(url)
+        data = r.json()
+
+        events = data["events"]
+
+        for e in events:
+
+            league = e["tournament"]["name"]
+
+            if "U18" in league or "U19" in league or "U20" in league:
+                continue
+
+            home = e["homeTeam"]["name"]
+            away = e["awayTeam"]["name"]
+
+            minute = e["time"]["currentPeriodStartTimestamp"]
+
+            home_score = e["homeScore"]["current"]
+            away_score = e["awayScore"]["current"]
+
+            if home_score == 0 and away_score == 0:
+
+                msg = f"""
+⚽ MATCH TROVATO
+
+{home} vs {away}
+
+Score: {home_score}-{away_score}
+
+League: {league}
+"""
+
+                send(msg)
+
+        time.sleep(60)
+
     except Exception as e:
         print(e)
-        time.sleep(30)
+        time.sleep(60)
