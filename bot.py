6251ee0020ffcf087cd88da25234022f31d3266f
antdepin <antdepin@gmail.com>
2026-03-15 19:09:13 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,17 @@
+import requests
+import time
+
+TOKEN = "1292804066:AAHIGsAOWz3vBXF4RJBnnQGH9m2UgNfJhek"
+CHAT_ID = "178689360"
+
+def send(msg):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+
+print("BOT PARTITO")
+
+send("Bot LIVE avviato su Railway")
+
 while True:
     try:
         print("Scanner attivo")
