3dafdbe3e086b735ec6b9a645112dc1a211c16d6
antdepin <antdepin@gmail.com>
2026-03-15 19:12:02 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,74 @@
 import requests
 import time
+from bs4 import BeautifulSoup
 
 TOKEN = "1292804066:AAHIGsAOWz3vBXF4RJBnnQGH9m2UgNfJhek"
 CHAT_ID = "178689360"
+
+BLACKLIST = [
+"u18",
+"u19",
+"u20",
+"youth",
+"women"
+]
+
+sent_matches = set()
 
 def send(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
 
 print("BOT PARTITO")
+send("🚀 Asian Scanner LIVE avviato")
 
-send("Bot LIVE avviato su Railway")
+def get_matches():
+
+    url = "https://www.nowgoal.com/"
+    r = requests.get(url, timeout=20)
+
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    matches = []
+
+    rows = soup.find_all("tr")
+
+    for row in rows:
+
+        text = row.get_text(" ").lower()
+
+        if "vs" in text:
+
+            league = text
+
+            if any(word in league for word in BLACKLIST):
+                continue
+
+            matches.append(text)
+
+    return matches
+
 
 while True:
+
     try:
+
         print("Scanner attivo")
-        send("scanner attivo")
+
+        matches = get_matches()
+
+        for match in matches:
+
+            if match not in sent_matches:
+
+                send(f"⚽ LIVE MATCH\n\n{match}")
+
+                sent_matches.add(match)
+
         time.sleep(30)
 
     except Exception as e:
-        print(e)
+
+        print("Errore:", e)
+
         time.sleep(30)
