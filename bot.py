ec4b7021cdf5f6a9d1550b1fa8a3959fdf33a7eb
antdepin <antdepin@gmail.com>
2026-03-15 17:51:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,25 @@
+import requests
 import time
 
+TOKEN="8687563836:AAE8RXxf2-UWxbr7U5cFscJ8Bri6NflIS6Q"
+CHAT_ID="178689360"
+
+def send(msg):
+    url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url,data={"chat_id":CHAT_ID,"text":msg})
+
 while True:
-    print("Asian bot running...")
-    time.sleep(30)
+
+    try:
+
+        r=requests.get("https://www.nowgoal.com/football/live")
+
+        if "football" in r.text:
+
+            print("scanner live")
+
+        time.sleep(30)
+
+    except:
+
+        time.sleep(30)
