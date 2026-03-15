d036c848099b8640e67c4af05f1edb25efd0c741
antdepin <antdepin@gmail.com>
2026-03-15 18:41:25 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,21 @@
-import asyncio
 import requests
-from playwright.async_api import async_playwright
+import time
 
 TOKEN = "1292804066:AAHIGsAOWz3vBXF4RJBnnQGH9m2UgNfJhek"
 CHAT_ID = "178689360"
 
 def send(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+    requests.post(url, data={"chat_id": CHAT_ID, "text": msg}, timeout=20)
 
-async def scan():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True)
-        page = await browser.new_page()
+print("BOT PARTITO")
 
-        await page.goto("https://www.asianodds.com/en", timeout=60000)
+send("🚀 Bot LIVE avviato su Railway")
 
-        text = await page.content()
-
-        if "OU Points" in text:
-            send("🔥 AsianOdds scanner attivo")
-
-        await browser.close()
-
-async def main():
-    while True:
-        try:
-            await scan()
-        except Exception as e:
-            print(e)
-
-        await asyncio.sleep(30)
-
-asyncio.run(main())
+while True:
+    try:
+        print("Scanner attivo")
+        time.sleep(30)
+    except Exception as e:
+        print(e)
+        time.sleep(30)
